ef4f78aa2e3485c34ac8e4d2e61e540f8cb116e8
gertagosa <121582460+gertagosa@users.noreply.github.com>
2023-01-05 12:07:22 +0000

    Added the search engine

--- src/app.js
+++ src/app.js
@@ -43,9 +43,19 @@ function displayTemperature(response) {
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
+function search(city) {
+  let apiKey = "2a7b71a1d85f2bddd00c80da9c16acb1";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-let apiKey = "2a7b71a1d85f2bddd00c80da9c16acb1";
-let city = "London";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+search("London");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
