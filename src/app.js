862cef34cee9551429f0412cf9caf5b9b109f5cb
gertagosa <121582460+gertagosa@users.noreply.github.com>
2023-01-06 22:17:27 +0000

    Added JS template

--- src/app.js
+++ src/app.js
@@ -21,6 +21,35 @@ function formatDate(timestamp) {
   let day = days[date.getDay()];
   return `${day} ${hours} ${minutes}`;
 }
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  let days = ["Thur", "Fri", "Sat", "Sun"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+      <div class="col-2">
+      <div class=" weather-forecast-date">${day}</div>
+      <img
+        src="https://openweathermap.org/img/wn/04n@2x.png"
+        alt=""
+        width="42"
+      />
+      <div class="weather-forecast-temperature">
+        <span class="weather-forecast-temperature-max">14° </
+        span>
+        <span class="weather-forecast-temperature-min">7° </
+        span>
+      </div>
+    </div>
+  
+  `;
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
 
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
@@ -86,3 +115,5 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("London");
+
+displayForecast();
